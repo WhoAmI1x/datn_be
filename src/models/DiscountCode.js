b811fbf06ceddfb3aef78beb193b0e491e6a26c1
Heracus <trantrunghuynh1998@gmail.com>
2021-06-30 18:01:51 +0700

    Format

--- src/models/DiscountCode.js
+++ src/models/DiscountCode.js
@@ -1,40 +1,18 @@
 const { Schema, model, models } = require("mongoose");
 
 const discountCodeSchema = new Schema({
-    ecommerce: {
-        type: String
-    },
+    ecommerce: { type: String },
     expires: Number,
-    code: {
-        type: String
-    },
+    code: { type: String },
     mainId: Number,
     tikiRuleId: Number,
-    shopeeSignature: {
-        type: String
-    },
-    imageUrl: {
-        type: String
-    },
-    title: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    categoryId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Category'
-    },
-    shopId: {
-        type: Number
-    },
-    userIds: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
+    shopeeSignature: { type: String },
+    imageUrl: { type: String },
+    title: { type: String },
+    description: { type: String },
+    categoryId: { type: Schema.Types.ObjectId, ref: 'Category' },
+    shopId: { type: Number },
+    userIds: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     markTime: { type: Number }
 });
 
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -4,13 +4,13 @@ const ProductControllers = require("../controllers/product.controllers");
 const auth = require("../middlewares/auth");
 const router = express.Router();
 
-router.get("/get-products-by-category", auth, asyncWrap(ProductControllers.getProductsByCategory))
+router.get("/get-products-by-category", auth, asyncWrap(ProductControllers.getProductsByCategory));
 
 router.get("/get-product-detail", auth, asyncWrap(ProductControllers.getDetailProduct));
 
 router.delete("/delete-product", auth, asyncWrap(ProductControllers.deleteProduct));
 
-router.get("/search-product", auth, asyncWrap(ProductControllers.searchProduct))
+router.get("/search-product", auth, asyncWrap(ProductControllers.searchProduct));
 
 router.get("/get-product-detail-searched", auth, asyncWrap(ProductControllers.getDetailProductSearched));
 
